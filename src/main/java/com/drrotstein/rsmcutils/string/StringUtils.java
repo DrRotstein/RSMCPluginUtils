e6b71fde723eaf6a7e1ac9bea06eade362ae4aa0
DrRotstein <finn.silas.roth@gmail.com>
2023-02-05 20:44:53 +0100

    Added validateName method to StringUtils

--- src/main/java/com/drrotstein/rsmcutils/string/StringUtils.java
+++ src/main/java/com/drrotstein/rsmcutils/string/StringUtils.java
@@ -9,4 +9,15 @@ public class StringUtils {
 								LETTERS_NUMBERS = LETTERS + NUMBERS,
 								MC_NAMING = LETTERS_NUMBERS + "_";
 	
+	/**
+	 * Returns true, if all the characters of {@link in} are contained in {@link allowed}
+	 * @param in The string to be validated
+	 * @param allowed The allowed characters
+	 * @return True, if all the characters of {@link in} are contained in {@link allowed}
+	 */
+	public static boolean validateName(String in, String allowed) {
+		for(char c : in.toCharArray()) if(!allowed.contains(c + "")) return false;
+		return true;
+	}
+	
 }
