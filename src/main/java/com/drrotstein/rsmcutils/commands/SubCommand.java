0040b2581a8fdd4a29e55c8b803b7a83d2c8201e
DrRotstein <finn.silas.roth@gmail.com>
2023-02-05 12:06:20 +0100

    Improved help message formatting for subcommands

--- src/main/java/com/drrotstein/rsmcutils/commands/SubCommand.java
+++ src/main/java/com/drrotstein/rsmcutils/commands/SubCommand.java
@@ -1,13 +1,16 @@
 package com.drrotstein.rsmcutils.commands;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public abstract class SubCommand extends SubCommandParent {
 	
 	public abstract String getLabel();
 	public abstract String getPermission();
 	
 	@Override
-	public String getHelpMessage() {
-		return null;
+	public List<String> getHelpMessage() {
+		return new ArrayList<>();
 	}
 	
 }
